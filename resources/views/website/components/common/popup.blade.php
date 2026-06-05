87b5fb6e6106b8342311d62bbf09b013af16eeda
gracychadha <chadhagracy8@gmail.com>
2026-06-05 13:49:07 +0530

    popup

---
+++ resources/views/website/components/common/popup.blade.php
@@ -0,0 +1,7 @@
+<div id="landingPopup" class="landing-popup">
+    <div class="popup-content">
+        <span class="popup-close">&times;</span>
+
+        <img src="{{ asset('website/images/popup-banner.jpeg') }}" alt="Popup Banner">
+    </div>
+</div>  
