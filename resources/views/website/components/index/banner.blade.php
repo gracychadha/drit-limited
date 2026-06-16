e70d68dbcea4786a691fe7e1497fd7e43f297e26
gracychadha <chadhagracy8@gmail.com>
2026-06-16 17:57:21 +0530

    mobile update

--- resources/views/website/components/index/banner.blade.php
+++ resources/views/website/components/index/banner.blade.php
@@ -4,11 +4,11 @@
             <rs-slide data-key="rs-1" data-title="Slide" data-thumb="website/images/upload/banner-01.png"
                 data-anim="ei:d;eo:d;s:d;r:0;t:fade;sl:d;">
 
-                <img src="{{ asset('website/images/upload/banner-01.png') }}" title="slider-img-03.jpg"
-                    width="1920" height="693" class="rev-slidebg tp-rs-img" data-no-retina="">
+                <img src="{{ asset('website/images/upload/banner-01.png') }}" title="slider-img-03.jpg" width="1920"
+                    height="693" class="rev-slidebg tp-rs-img" data-no-retina="">
 
                 <rs-layer id="slider-1-slide-1-layer-0" data-type="text" data-color="#fff" data-rsp_ch="on"
-                    data-xy="x:r,r,c,c;xo:35px,35px,0,0;yo:91px,85px,-17px,-20px;"
+                    data-xy="x:r,r,c,c;xo:35px,35px,0,0;yo:91px,85px,40px,80px;"
                     data-text="w:normal;s:18,18,18,18;l:28,28,28,28;fw:700;" data-dim="minh:0px,0px,none,none;"
                     data-padding="t:6,6,6,5;r:20,20,19,12;b:6,6,6,5;l:20,20,19,12;" data-border="bor:0px,0px,0px,0px;"
                     data-frame_0="y:-100%;" data-frame_0_mask="u:t;" data-frame_1="st:190;sp:1200;sR:190;"
@@ -18,7 +18,7 @@
 
 
                 <rs-layer id="slider-1-slide-1-layer-1" data-type="text" data-color="#05255f" data-rsp_ch="on"
-                    data-xy="x:r,r,c,c;xo:34px,34px,0,0;yo:130px,110px,20px,20px;"
+                    data-xy="x:r,r,c,c;xo:34px,34px,0,0;yo:130px,110px,110px,120px;"
                     data-text="w:normal;s:72,72,60,50;l:92,92,80,70;fw:400;" data-frame_0="y:-100%;"
                     data-frame_0_mask="u:t;" data-frame_1="st:310;sp:1200;sR:310;" data-frame_1_mask="u:t;"
                     data-frame_999="o:0;st:w;sR:7490;" style="z-index:10;font-family: 'Lato', sans-serif;">Focused on
@@ -27,7 +27,7 @@
                 </rs-layer>
 
                 <rs-layer id="slider-1-slide-1-layer-2" data-type="text" data-color="#05255f" data-rsp_ch="on"
-                    data-xy="x:r,r,c,c;xo:34px,34px,0,1px;yo:200px,180px,40px,40px;"
+                    data-xy="x:r,r,c,c;xo:34px,34px,0,0;yo:200px,180px,150px,180px;"
                     data-text="w:normal;s:72,72,60,50;l:92,92,80,70;fw:700;" data-frame_0="y:-100%;"
                     data-frame_0_mask="u:t;" data-frame_1="st:440;sp:1200;sR:440;" data-frame_1_mask="u:t;"
                     data-frame_999="o:0;st:w;sR:7360;" style="z-index:11; font-family: 'Lato', sans-serif;">Matters
@@ -35,8 +35,8 @@
                 </rs-layer>
 
                 <rs-layer id="slider-1-slide-1-layer-3" data-type="text" data-color="#676b72" data-rsp_ch="on"
-                    data-xy="x:r,r,c,c;xo:34px,34px,0,666px;yo:300px, 280px,256px,233px;"
-                    data-text="w:normal;s:18,18,18,15;l:26,26,26,25;" data-vbility="t,t,f,f" data-frame_0="y:-100%;"
+                    data-xy="x:r,r,c,c;xo:34px,34px,0,666px;yo:300px, 280px,200px,230px;"
+                    data-text="w:normal;s:18,18,18,15;l:26,26,26,25;"  data-frame_0="y:-100%;"
                     data-frame_0_mask="u:t;" data-frame_1="st:680;sp:1200;sR:680;" data-frame_1_mask="u:t;"
                     data-frame_999="o:0;st:w;sR:7120;" style="z-index:12;font-family: 'Lato', sans-serif;">We put
                     clients first—delivering tailored
@@ -46,27 +46,28 @@
             <rs-slide data-key="rs-4" data-title="Slide" data-thumb="website/images/upload/banner-02.png"
                 data-anim="ei:d;eo:d;s:d;r:0;t:fade;sl:d;">
 
-                <img src="{{ asset('website/images/upload/banner-02.png') }}" title="slider-img-02.jpg"
-                    width="1920" height="693" class="rev-slidebg tp-rs-img" data-no-retina="">
+                <img src="{{ asset('website/images/upload/banner-02.png') }}" title="slider-img-02.jpg" width="1920"
+                    height="693" class="rev-slidebg tp-rs-img" data-no-retina="">
 
 
                 <rs-layer id="slider-1-slide-4-layer-0" data-type="text" data-color="#b30000" data-rsp_ch="on"
-                    data-xy="x:l,l,c,c;xo:35px,35px,0,0;yo:91px,85px,-17px,-20px;"
+                    data-xy="x:r,r,c,c;xo:35px,35px,0,0;yo:91px,85px,40px,80px;"
                     data-text="w:normal;s:16,16,16,16;l:26,26,26,26;fw:700;" data-frame_0="y:-100%;"
                     data-frame_0_mask="u:t;" data-frame_1="st:190;sp:1200;sR:190;" data-frame_1_mask="u:t;"
-                    data-frame_999="o:0;st:w;sR:7610;" style="z-index:9;font-family: 'Lato', sans-serif;">Value-Driven Excellence
+                    data-frame_999="o:0;st:w;sR:7610;" style="z-index:9;font-family: 'Lato', sans-serif;">Value-Driven
+                    Excellence
                 </rs-layer>
 
                 <rs-layer id="slider-1-slide-4-layer-1" data-type="text" data-color="#05255f" data-rsp_ch="on"
-                    data-xy="x:l,l,c,c;xo:35px,35px,0,0;yo:130px,110px,20px,20px;"
+                    data-xy="x:r,r,c,c;xo:34px,34px,0,0;yo:130px,110px,110px,120px;"
                     data-text="w:normal;s:72,72,60,50;l:92,92,80,70;fw:400;" data-frame_0="y:-100%;"
                     data-frame_0_mask="u:t;" data-frame_1="st:310;sp:1200;sR:310;" data-frame_1_mask="u:t;"
                     data-frame_999="o:0;st:w;sR:7490;" style="z-index:10;font-family:'Lato', sans-serif;">Delivering
-                        Quality
+                    Quality
                 </rs-layer>
 
                 <rs-layer id="slider-1-slide-4-layer-2" data-type="text" data-color="#05255f" data-rsp_ch="on"
-                    data-xy="x:l,l,c,c;xo:35px,35px,0,1px;yo:200px,180px,40px,40px;"
+                    data-xy="x:r,r,c,c;xo:34px,34px,0,0;yo:200px,180px,150px,180px;"
                     data-text="w:normal;s:72,72,60,50;l:92,92,80,70;fw:700;" data-frame_0="y:-100%;"
                     data-frame_0_mask="u:t;" data-frame_1="st:440;sp:1200;sR:440;" data-frame_1_mask="u:t;"
                     data-frame_999="o:0;st:w;sR:7360;" style="z-index:11;font-family:'Lato', sans-serif;">You Can <span
@@ -75,10 +76,11 @@
 
                 <rs-layer id="slider-1-slide-4-layer-3" data-type="text" data-color="#676b72" data-rsp_ch="on"
                     data-xy="x:l,l,c,c;xo:35px,35px,0,666px;yo:300px, 280px,256px,233px;"
-                    data-text="w:normal;s:18,18,18,15;l:26,26,26,25;" data-vbility="t,t,f,f" data-frame_0="y:-100%;"
+                    data-text="w:normal;s:18,18,18,15;l:26,26,26,25;"  data-frame_0="y:-100%;"
                     data-frame_0_mask="u:t;" data-frame_1="st:680;sp:1200;sR:680;" data-frame_1_mask="u:t;"
-                    data-frame_999="o:0;st:w;sR:7120;" style="z-index:12;font-family:'Lato', sans-serif;">We provide value-based, high-quality services
-                        that meet professional<br> standards while ensuring exceptional results for every client.
+                    data-frame_999="o:0;st:w;sR:7120;" style="z-index:12;font-family:'Lato', sans-serif;">We provide
+                    value-based, high-quality services
+                    that meet professional<br> standards while ensuring exceptional results for every client.
                 </rs-layer></rs-slide>
         </rs-slides>
     </rs-module>
