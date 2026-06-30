aa0f57134908dc2b644f45b574494e8345bc8d60
gracychadha <chadhagracy8@gmail.com>
2026-06-30 14:11:01 +0530

    gallery

--- resources/views/website/components/our-gallery/gride.blade.php
+++ resources/views/website/components/our-gallery/gride.blade.php
@@ -3,79 +3,61 @@
     <div class="container">
         <!-- row -->
         <div class="row mt_15 mb_15">
-            <div class=" col-lg-4 col-md-6 col-sm-6 mb-2">
-                <!-- featured-imagebox-portfolio -->
+            <div class=" col-lg-12 col-md-6 col-sm-6 mb-2">
+
                 <div class="featured-imagebox featured-imagebox-portfolio style2">
                     <div class="featured-thumbnail">
                         <img width="700" height="800" class="img-fluid"
-                            src="{{ asset('website/images/upload/event (1).jpeg') }}" alt="portfolio_img">
+                            src="{{ asset('website/images/upload/dritm-gallery.png') }}" alt="portfolio_img">
                     </div>
-                    
-                    <div class="featured-content">
-                        <div class="category">
-                            <span>Event</span>
-                        </div>
-                        <div class="featured-title">
-                            <h3><a href="#">Dr ITM Group</a></h3>
-                        </div>
-                    </div>
-                </div><!-- featured-imagebox-portfolio end-->
+
+
+                </div>
             </div>
             <div class=" col-lg-4 col-md-6 col-sm-6 mb-2">
-                <!-- featured-imagebox-portfolio -->
+
                 <div class="featured-imagebox featured-imagebox-portfolio style2">
                     <div class="featured-thumbnail">
                         <img width="700" height="800" class="img-fluid"
                             src="{{ asset('website/images/upload/event (2).jpeg') }}" alt="portfolio_img">
                     </div>
-                    
-                    <div class="featured-content">
-                        <div class="category">
-                            <span>Event</span>
-                        </div>
-                        <div class="featured-title">
-                            <h3><a href="#">Dr ITM Group</a></h3>
-                        </div>
-                    </div>
-                </div><!-- featured-imagebox-portfolio end-->
+
+
+                </div>
             </div>
             <div class=" col-lg-4 col-md-6 col-sm-6 mb-2">
-                <!-- featured-imagebox-portfolio -->
+
                 <div class="featured-imagebox featured-imagebox-portfolio style2">
                     <div class="featured-thumbnail">
                         <img width="700" height="800" class="img-fluid"
                             src="{{ asset('website/images/upload/event (3).jpeg') }}" alt="portfolio_img">
                     </div>
-                   
-                    <div class="featured-content">
-                        <div class="category">
-                            <span>Event</span>
-                        </div>
-                        <div class="featured-title">
-                            <h3><a href="#">Dr ITM Group</a></h3>
-                        </div>
-                    </div>
-                </div><!-- featured-imagebox-portfolio end-->
+
+
+                </div>
             </div>
             <div class=" col-lg-4 col-md-6 col-sm-6 mb-2">
-                <!-- featured-imagebox-portfolio -->
+
                 <div class="featured-imagebox featured-imagebox-portfolio style2">
                     <div class="featured-thumbnail">
                         <img width="700" height="800" class="img-fluid"
                             src="{{ asset('website/images/upload/event (4).jpeg') }}" alt="portfolio_img">
                     </div>
-                   
-                    <div class="featured-content">
-                        <div class="category">
-                            <span>Event</span>
-                        </div>
-                        <div class="featured-title">
-                            <h3><a href="#">Dr ITM Group</a></h3>
-                        </div>
-                    </div>
-                </div><!-- featured-imagebox-portfolio end-->
-            </div>
 
+
+                </div>
+            </div>
+            <div class=" col-lg-12 col-md-6 col-sm-6 mb-4">
+
+                <div class="featured-imagebox featured-imagebox-portfolio style2">
+                    <div class="featured-thumbnail">
+                        <img width="700" height="800" class="img-fluid"
+                            src="{{ asset('website/images/upload/dritm-gallery.png') }}" alt="portfolio_img">
+                    </div>
+
+
+                </div>
+            </div>
 
         </div>
         <!-- row end -->
